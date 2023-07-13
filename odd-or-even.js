d1d27d4a441724dac24fddd07ceb6a77d6aeb57c
m-diana-dev <m.diana.dev@gmail.com>
2023-07-13 16:36:19 +0300

    Odd or Even?

---
+++ odd-or-even.js
@@ -0,0 +1,8 @@
+const oddOrEven = (array) => {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    if (array.length === 0 || sum % 2 === 0) return 'even'
+    else return 'odd'
+}
