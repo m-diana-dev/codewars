5818ab0c75bb87d52e6a833323ca3f7567b1aae3
m-diana-dev <m.diana.dev@gmail.com>
2023-07-28 22:41:30 +0300

    fix - Basic Mathematical Operations (8kyu)

--- mathematical-operations.js
+++ mathematical-operations.js
@@ -22,5 +22,7 @@ function basicOp(operation, value1, value2)
             return value1*value2;
         case '/':
             return value1/value2;
+        default:
+            return 0;
     }
 }
