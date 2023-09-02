ffd286a8991d72cba37803d2fcfb22dcb809d1f7
m-diana-dev <m.diana.dev@gmail.com>
2023-09-02 22:20:02 +0300

    Cats and shelves

---
+++ cats-and-shelves.js
@@ -0,0 +1,45 @@
+/*
+7 kyu
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump either one or three shelves at a time: from shelf i to shelf i+1 or i+3 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐
+│------5-│
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │
+BANG!────┘  ├─────► OK!
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+Input
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+
+Task
+Find the minimum number of jumps to go from start to finish
+
+Example
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+ */
+
+const solution = (start, finish) => {
+    let count = 0;
+    for (let i = start; i < finish;) {
+        if (i <= finish - 3) {
+            count++;
+            i += 3
+        } else {
+            count++;
+            i++
+        }
+    }
+    return count
+}
