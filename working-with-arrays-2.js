481be4121abf4f315735caf0e1576da5755d5d41
m-diana-dev <m.diana.dev@gmail.com>
2023-10-22 21:10:07 +0300

    Working with arrays I (and why your code fails in some katas)

---
+++ working-with-arrays-2.js
@@ -0,0 +1,23 @@
+/*
+7 kyu
+
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+Some good reading: MDN Docs about arrays
+
+function withoutLast(arr) {
+  // Fix it
+  arr.pop(); // removes the last element
+  return arr;
+}
+ */
+
+const withoutLast = (arr) => {
+    const arrCopy = [...arr]
+    arrCopy.pop()
+    return arrCopy
+}
